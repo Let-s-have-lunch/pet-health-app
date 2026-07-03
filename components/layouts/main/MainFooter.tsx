c43fa886677cbb9a28a2eecfeebf80f9009f4e2b
mcm <joohyuna2@gmail.com>
2026-07-03 15:43:44 +0900

    팔레트 수정 텍스트 포인트 추가 했습니다.

--- components/layouts/main/MainFooter.tsx
+++ components/layouts/main/MainFooter.tsx
@@ -1,9 +1,14 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, View } from "react-native";
 import { twMerge } from "tailwind-merge";
-import { Ionicons } from "@expo/vector-icons";
+import { Feather, Ionicons } from "@expo/vector-icons";
 import TextComponent from "@/components/common/text/TextComponents";
+import { USER_NAV_LIST } from "@/constants/menu";
+import { Link, usePathname } from "expo-router";
+import { useAuthStore } from "@/stores/auth/useAuthStore";
 
 function MainFooter() {
+    const pathname = usePathname();
+    const { user, logout } = useAuthStore();
     return (
         <View
             className={twMerge([
@@ -13,21 +18,25 @@ function MainFooter() {
                 "justify-between",
                 "items-center",
                 "px-7",
-                "bg-background-paper"
+                "bg-background-paper",
             ])}>
-            <Pressable className={twMerge(["justify-center", "items-center"])}>
-                <Ionicons name={"home-outline"} size={24} className={"text-text-default"} />
-                <TextComponent className={""}>홈</TextComponent>
-            </Pressable>
-            <Pressable className={twMerge(["justify-center", "items-center"])}>
-                <Ionicons name={"home-outline"} size={24} className={"text-text-default"} />홈
-            </Pressable>
-            <Pressable className={twMerge(["justify-center", "items-center"])}>
-                <Ionicons name={"home-outline"} size={24} className={"text-text-default"} />홈
-            </Pressable>
-            <Pressable className={twMerge(["justify-center", "items-center"])}>
-                <Ionicons name={"home-outline"} size={24} className={"text-text-default"} />홈
-            </Pressable>
+            {USER_NAV_LIST.map(item => {
+                const isActive =
+                    item.path === "/" ? pathname === "/" : pathname.startsWith(item.path);
+
+                return (
+                    <Link href={item.path} key={item.path} asChild>
+                        <Pressable className={twMerge(["justify-center", "items-center"])}>
+                            <Feather
+                                name={item.icon as any}
+                                size={24}
+                                className={"text-text-default"}
+                            />
+                            <TextComponent>{item.label}</TextComponent>
+                        </Pressable>
+                    </Link>
+                );
+            })}
         </View>
     );
 }
