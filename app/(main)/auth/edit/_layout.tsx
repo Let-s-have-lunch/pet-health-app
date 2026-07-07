1d41160fb3b2be096b1467896faf4f4955234cc8
DeveloperSky95 <haneulgim95@gmail.com>
2026-07-07 14:46:13 +0900

    auth/edit/_layout.tsx수정

--- app/(main)/auth/edit/_layout.tsx
+++ app/(main)/auth/edit/_layout.tsx
@@ -1,14 +1,23 @@
 import { Stack, Redirect } from "expo-router";
 import { useAuthStore } from "@/stores/auth/useAuthStore";
+import LoadingIndicator from "@/components/common/loading/LoadingIndicator";
 
 export default function ProtectedLayout() {
-    const { user } = useAuthStore();
+    const { user, isInitialized } = useAuthStore();
 
-    // 유저가 없다면 하위 페이지들을 아예 렌더링하지 않고 로그인 페이지로 튕겨냄
+    if (!isInitialized) {
+        return <LoadingIndicator />;
+    }
+
     if (!user) {
         return <Redirect href="/auth/login" />;
     }
 
-    // 유저가 있다면 정상적으로 스택을 렌더링함
-    return <Stack />;
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        />
+    );
 }
