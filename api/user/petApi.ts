1cc0ee0dae2ae3ee103ed84c0df4dc095765c7d1
yeonhan <yeonhanlab@gmail.com>
2026-07-08 11:07:26 +0900

    petApi 작성

--- api/user/petApi.ts
+++ api/user/petApi.ts
@@ -1,6 +1,7 @@
 import axiosInstance from "@/api/axiosInstance";
 import { RegisterPetInputType } from "@/schemas/user/pet/registerPetSchema";
 import { Pet } from "@/types/pet";
+import { PetUpdateInputType } from "@/schemas/user/pet/updatePetSchema";
 
 const getMyPetList = async():Promise<Pet[]> => {
     const response = await axiosInstance.get("/pet/list");
@@ -12,10 +13,20 @@ const registerPet = async ( data: RegisterPetInputType ): Promise<Pet> => {
     return response.data.data;
 };
 
+const updatePet = async ( petId: number, data: PetUpdateInputType ): Promise<Pet> => {
+    const response = await axiosInstance.patch(`/pet/update/${petId}`, data);
+    return response.data.data;
+};
+
+const deletePet = async ( petId: number ) => {
+    await axiosInstance.delete(`/pet/delete/${petId}`);
+}
 
 
 
 export default {
     getMyPetList,
     registerPet,
+    updatePet,
+    deletePet,
 };
