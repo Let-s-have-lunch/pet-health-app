e665bb7a765f034373bc0e8a527c55912780b7b3
yeonhan <yeonhanlab@gmail.com>
2026-07-13 11:16:22 +0900

    오류 수정

--- components/history/HistorySection.tsx
+++ components/history/HistorySection.tsx
@@ -173,7 +173,7 @@ export default function HistorySection() {
 
     return (
         <ScrollView className="flex-1 pt-1.8 bg-background-default">
-            <View className="flex-row flex-wrap justify-between px-5 mt-6">
+            <View className="flex-row flex-wrap justify-between mt-6">
                 {cardConfig.map(card => (
                     <Pressable
                         key={card.id}
