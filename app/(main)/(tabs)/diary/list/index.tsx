0c453532db4bf9263a5b79d393155040446b147e
DeveloperSky95 <haneulgim95@gmail.com>
2026-07-15 15:38:29 +0900

    DailyDetailScreen 페이지 작업중

--- app/(main)/(tabs)/diary/list/index.tsx
+++ app/(main)/(tabs)/diary/list/index.tsx
@@ -1,5 +1,55 @@
-import { Text, View } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { View, ScrollView } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import todoApi from "@/api/user/todoApi";
+import diaryApi from "@/api/user/diaryApi";
+import { Diary } from "@/types/diary";
+import { Todo } from "@/types/todo";
+import LoadingIndicator from "@/components/common/loading/LoadingIndicator";
+import ContentContainer from "@/components/layouts/common/ContentContainer";
 
-export default function DiaryListScreen() {
-    return <View><Text>목록출력</Text></View>
+export default function DailyDetailScreen() {
+    const { date } = useLocalSearchParams<{ date: string }>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [diaryData, setDiaryData] = useState<Diary[]>([]);
+    const [todoData, setTodoData] = useState<Todo[]>([]);
+
+    const fetchDailyData = useCallback(async () => {
+        try {
+            const [diaries, todos] = await Promise.all([
+                diaryApi.getDiaryList(date),
+                todoApi.getTodoList(date),
+            ]);
+
+            setDiaryData(diaries);
+            setTodoData(todos);
+        } catch (error) {
+            console.error("데이터 로드 실패:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [date]);
+
+    useEffect(() => {
+        if (date) fetchDailyData().then(() => {});
+    }, [date, fetchDailyData]);
+
+    if (isLoading) {
+        return (
+            <View className="py-20">
+                <LoadingIndicator />
+            </View>
+        );
+    }
+
+    return (
+        <View className="flex-1">
+            <ScrollView className={"flex-1"}>
+                <ContentContainer className={"p-0"}>
+                    {/*<DiaryComponent diaries={diaryData} />*/}
+                    {/*<TodoComponent todos={todoData} targetDate={date} />*/}
+                </ContentContainer>
+            </ScrollView>
+        </View>
+    );
 }
