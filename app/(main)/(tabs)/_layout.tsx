9e5db5b495858b35807efa4c70a88beac900274d
mcm <joohyuna2@gmail.com>
2026-07-16 17:59:28 +0900

    tab 레이아웃 수정

--- app/(main)/(tabs)/_layout.tsx
+++ app/(main)/(tabs)/_layout.tsx
@@ -1,26 +1,21 @@
 import { ScrollView, View } from "react-native";
-import { Slot, usePathname, useRouter, useSegments } from "expo-router";
+import { Slot, usePathname, useRouter } from "expo-router";
 import MainHeaderMobile from "@/components/layouts/main/MainHeaderMobile";
 import MainFooter from "@/components/layouts/main/MainFooter";
 import ContentContainer from "@/components/layouts/common/ContentContainer";
-import MyPageHeader from "@/components/layouts/main/MyPageHeader";
 import Title from "@/components/common/title/Title";
+import ProfileHeader from "@/components/layouts/main/ProfileHeader";
 
 function MainLayout() {
-    const segments = useSegments();
     const pathname = usePathname();
     const router = useRouter();
 
-    const currentSegment = segments[segments.length - 1];
-    const isPostsPage = currentSegment === "post";
     const isDiaryListPage = pathname.includes("/diary/list");
     const isProfilePage = pathname.includes("/profile");
 
     return (
         <View className={"flex-1 bg-background-default"}>
-            {isPostsPage ? (
-                <PostPageHeader />
-            ) : isDiaryListPage ? (
+            {isDiaryListPage ? (
                 <Title
                     title="일기 목록"
                     showBackButton={true}
